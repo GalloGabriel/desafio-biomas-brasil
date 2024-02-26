d34fb35d7384bcb53637b58a3f2c087d64b9ad31
GalloGabriel <gabriel.gallo2242@gmail.com>
2024-02-26 16:37:19 -0300

    ajustando loading quiz

--- javascripts/events.js
+++ javascripts/events.js
@@ -2,14 +2,10 @@ const tituloCuriosidades = document.querySelector('.titulo-curiosidades');
 const nextContentContainer = document.querySelector('.next-content-container');
 const textoCuriosidade = document.querySelector('.texto-curiosidade');
 const nextContentItem = document.querySelectorAll('.next-content');
-const BASE_URL_IMAGE = "https://apps.univesp.br/_testes/desafio-biomas-do-brasil/assets/"
+const BASE_URL_IMAGE = "https://apps.univesp.br/_testes/desafio-biomas-do-brasil/assets/";
 
-let imagesCuriosidades = new Image();
-arraySrcImages = ["tela-amaz.png", ""]
-imagesCuriosidades.src = `${BASE_URL_IMAGE}tela-pampas.png`;
-imagesCuriosidades.onload = function (){
-  $('.curiosidadesContainer').css("display", "flex")
-}
+
+let arraySrcImages = ["tela-amaz.png", "tela-pantanal.png", "tela-cerrado.png", "tela-caatinga.png", "tela-mata.png", "tela-pampas.png"];
 
 // Definindo qual o caminho da URL atual
 const { pathname } = window.location;
@@ -18,19 +14,38 @@ let arrayBiomas = [ 'amazonia', 'pantanal', 'cerrado', 'caatinga', 'mata-atlanti
 let arrayCuriosidadesBiomas = [ curiosidadesAmazonia, curiosidadesPantanal, curiosidadesCerrado, curiosidadesCaatinga, curiosidadesMataAtlantica, curiosidadesPampas ];
 
 for (let i = 0; i < arrayBiomas.length + 1; i++) {
+  
   if(pathname === `/quiz/${arrayBiomas[i]}.html`){
 
-    // Troca automaticamente o conteudo das curiosidades dos biomas
-    arrayCuriosidadesBiomas[i].forEach(
-      (element, i) => {
-      setTimeout(() => {
-        tituloCuriosidades.innerHTML = element.titulo;
-        nextContentContainer.innerHTML = element.indicador;
-        textoCuriosidade.innerHTML = element.curiosidade;
-        
-      }, 5000 * i);
+    chamaLoading(`loading-${arrayBiomas[i]}`)
+
+    let imagesCuriosidades = new Image();
+    imagesCuriosidades.src = `${BASE_URL_IMAGE}${arraySrcImages[i]}`;
+    
+    
+    imagesCuriosidades.onload = function (){
+
+      loadingBox.style.display = 'none';
+
+      $('.curiosidadesContainer').css("display", "flex");
       
-    });
+      // Troca automaticamente o conteudo das curiosidades dos biomas
+      arrayCuriosidadesBiomas[i].forEach(
+        (element, i) => {
+        setTimeout(() => {
+          tituloCuriosidades.innerHTML = element.titulo;
+          nextContentContainer.innerHTML = element.indicador;
+          textoCuriosidade.innerHTML = element.curiosidade;
+          
+        }, 5000 * i);
+        
+      }); 
+      
+    }
+
+    
+
+    
     
   }
   
