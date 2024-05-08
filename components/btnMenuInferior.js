acf9972e091dfee1162fc0259a9734c1b1ef9f63
unknown <UNIVESP@PMD-1646Intelligency.local.univesp.br>
2024-05-08 15:55:36 -0300

    modal btn reiniciar

--- components/btnMenuInferior.js
+++ components/btnMenuInferior.js
@@ -3,5 +3,13 @@ let btnMenuContainerInferior = document.querySelector("#btnMenuInferior");
 btnMenuContainerInferior.innerHTML = `
 
 <div><img onmouseover="hoverImages(this, 'btn-menu_baixar-over')" onmouseout="tiraHover(this, 'btn-menu_baixar')" src="https://apps.univesp.br/_testes/desafio-biomas-do-brasil/assets/btn-menu_baixar.svg" alt="" class="btnConfig-baixar" ></div>
-<div><img onmouseover="hoverImages(this, 'btn-menu_reiniciar-over')" onmouseout="tiraHover(this, 'btn-menu_reiniciar')" src="https://apps.univesp.br/_testes/desafio-biomas-do-brasil/assets/btn-menu_reiniciar.svg" alt="" class="btnConfig-reiniciar" ></div>
+<div>
+    <img 
+        onmouseover="hoverImages(this, 'btn-menu_reiniciar-over')"  
+        onmouseout="tiraHover(this, 'btn-menu_reiniciar')" 
+        src="https://apps.univesp.br/_testes/desafio-biomas-do-brasil/assets/btn-menu_reiniciar.svg" 
+        alt="" 
+        class="btnConfig-reiniciar"
+        data-toggle="modal" 
+        data-target="#modalReiniciar"></div>
 <div><img onmouseover="hoverImages(this, 'btn-menu_cert-off')" onmouseout="tiraHover(this, 'btn-menu_cert-off')" src="https://apps.univesp.br/_testes/desafio-biomas-do-brasil/assets/btn-menu_cert-off.svg" alt="" class="btnConfig-cert" ></div>`;
